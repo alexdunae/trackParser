2b00484c87953b1e31790301332cfb2ad04eb5c8
Alex Dunae <alex@dunae.ca>
2020-01-27 16:16:01 -0800

    Use class for types

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,21 @@
 // Type definitions for trackParser
 // Project: trackParser
+// Definitions by: Alex Dunae <http://dunae.ca>
 
 export = TrackParser;
 
-declare function TrackParser(
-  rawTrackData: string
-): TrackParser.TrackParserResponse;
-
-declare namespace TrackParser {
-  export interface TrackParserResponse {
-    input_trackdata_str: string;
-    account_name: string | null;
-    account: string | null;
-    lastName: string | null;
-    firstName: string | null;
-    acccount: string | null;
-    expMonth: string | null;
-    expYear: string | null;
-    track1: string | null;
-    track2: string | null;
-    hasTrack1: boolean;
-    hasTrack2: boolean;
-  }
+declare class TrackParser {
+  constructor(rawTrackData: string);
+  input_trackdata_str: string;
+  account_name: string | null;
+  account: string | null;
+  lastName: string | null;
+  firstName: string | null;
+  acccount: string | null;
+  expMonth: string | null;
+  expYear: string | null;
+  track1: string | null;
+  track2: string | null;
+  hasTrack1: boolean;
+  hasTrack2: boolean;
 }
